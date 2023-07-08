b54736c551e9943f11a356ecc452e3935066fa5a
r34sfzr <124754877+r34sfzr@users.noreply.github.com>
2023-07-09 03:10:11 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# About
+Learning backend with dicoding beginner course. Using hapi framework and get 3 star for submission points.
---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addBookHandler,
+  getAllBookHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{Id}',
+    handler: deleteBookByIdHandler,
+  },
+];
+
+module.exports = routes;
